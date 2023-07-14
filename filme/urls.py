830b58797dd75c774462c3067619ed397c56cdd2
Rafael Rosa <rafael.ricardo@outlook.com>
2023-07-14 16:26:47 -0600

    nova pagina
    pagina de detalhes de filmes

--- filme/urls.py
+++ filme/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import Homepage, Homefilmes
+from .views import Homepage, Homefilmes, DetalhesFilme
 
 urlpatterns = [
     path('', Homepage.as_view(), name='homepage'),
     path('filmes/', Homefilmes.as_view(), name='homefilmes'),
+    path('filmes/<int:pk>', DetalhesFilme.as_view(), name='detalhesfilme'),
+    
 ]
