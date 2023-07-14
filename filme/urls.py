ddd96708e1023e430fcf7becc957a0d2921fc46f
Rafael Rosa <rafael.ricardo@outlook.com>
2023-07-14 17:15:47 -0600

    urls automatizadas
    urls automatizadas

--- filme/urls.py
+++ filme/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import Homepage, Homefilmes, DetalhesFilme
 
+app_name = 'filme'
+
 urlpatterns = [
     path('', Homepage.as_view(), name='homepage'),
     path('filmes/', Homefilmes.as_view(), name='homefilmes'),
